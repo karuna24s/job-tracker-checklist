bf4f56e4e468b29680bdd65b029ad37727269a45
karuna24s <karuna24s@gmail.com>
2017-03-03 14:37:22 -0500

    add app.js, updated layouts/application view and add ui-view tag to application/index view

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,12 @@
+(function() {
+
+
+  'use strict'
+
+  angular
+    .module('app', ['templates', 'ui.router', 'Devise', 'ngMessages'])
+    // .config(['$httpProvider', function($httpProvider) {
+    //     // for CSRF errors
+    //     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    // }]);
+}())
