206cc232a4d0a262541ab8ff6e8438dd10899816
karuna24s <karuna24s@gmail.com>
2017-03-02 19:27:14 -0500

    generated item resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   resources :checklists
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
