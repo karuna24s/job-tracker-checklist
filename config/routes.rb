fe4193beece6e30c09ec184f4c268e2e192e882a
karuna24s <karuna24s@gmail.com>
2017-03-02 20:00:09 -0500

    updated the routes for jobs, checklists and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :checklists
-  resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  resources :jobs do
+    resources :checklists
+  end
+
+  resources :checklists do
+    resources :items
+  end
 end
