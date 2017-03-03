1b2e259e3c4ff86f6e33b7841993ebd9d5deb5e0
karuna24s <karuna24s@gmail.com>
2017-03-02 19:38:35 -0500

    created item migration

--- db/migrate/20170303002638_create_items.rb
+++ db/migrate/20170303002638_create_items.rb
@@ -1,8 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      t.string :task
+      t.integer :checklist_id
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303001119) do
+ActiveRecord::Schema.define(version: 20170303002638) do
 
   create_table "checklists", force: :cascade do |t|
     t.integer  "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "task"
+    t.integer  "checklist_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "jobs", force: :cascade do |t|
