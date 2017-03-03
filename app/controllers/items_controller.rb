7b5873991d8cb5481dd0170a3698be2dcc5df9df
karuna24s <karuna24s@gmail.com>
2017-03-02 20:36:00 -0500

    implemented all the actions in the items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
 class ItemsController < ApplicationController
+
+  def index
+    items = Item.all
+    render json: items
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: { success: item, status: "success"}
+    else
+      render json: { errors: item.errors.full_messages,
+                     status: "error" }
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.save(item_params)
+      render json: { success: item, status: "success"}
+    else
+      render json: { errors: item.errors.full_messages,
+                    status: "error" }
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:task, :checklist_id)
+  end
+
 end
