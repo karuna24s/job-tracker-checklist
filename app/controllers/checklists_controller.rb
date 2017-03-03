5401108579348a8c0f77639d79f40846f6cdfb04
karuna24s <karuna24s@gmail.com>
2017-03-02 20:33:08 -0500

    implemented all the actions in the checklists_controller

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,2 +1,45 @@
 class ChecklistsController < ApplicationController
+
+  def index
+    checklists = Checklist.all
+    # debugger
+    render json: checklists
+  end
+
+  def show
+    checklist = Checklist.find(params[:id])
+    render json: checklist
+  end
+
+  def create
+    checklist = Checklist.new(checklist_params)
+    if checklist.save
+      render json: { success: checklist, status: "success"}
+    else
+      render json: { errors: checklist.errors.full_messages,
+                     status: "error" }
+    end
+  end
+
+  def update
+    checklist = Checklist.find(params[:id])
+    if checklist.save(checklist_params)
+      render json: { success: checklist, status: "success"}
+    else
+      render json: { errors: checklist.errors.full_messages,
+                    status: "error" }
+    end
+  end
+
+  def destroy
+    checklist = Checklist.find(params[:id])
+    checklist.destroy
+  end
+
+  private
+
+  def checklist_params
+    params.require(:checklist).permit(:job_id)
+  end
+
 end
