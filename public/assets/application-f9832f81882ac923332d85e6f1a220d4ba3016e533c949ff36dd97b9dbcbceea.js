409cd2f3a797ba93c098ad023c907487ca5d9b4e
karuna24s <karuna24s@gmail.com>
2017-03-31 07:24:27 -0400

    precompiled assets for production after adding older version of js controllers

--- public/assets/application-f9832f81882ac923332d85e6f1a220d4ba3016e533c949ff36dd97b9dbcbceea.js
+++ public/assets/application-f9832f81882ac923332d85e6f1a220d4ba3016e533c949ff36dd97b9dbcbceea.js
@@ -52228,40 +52228,45 @@ function ngMessageDirectiveFactory() {
 //       .factory('ChecklistFactory', ChecklistFactory);
 //
 // }());
-// (function(){
-//
-//   'use strict'
-//
-//   angular
-//     .module('app')
-//     .controller('ChecklistsController', ['ChecklistFactory', function(ChecklistFactory) {
-//     var vm = this;
-//
-//     vm.createChecklist = createChecklist;
-//
-//     activate();
-//
-//     function activate() {
-//       getChecklists();
-//     }
-//
-//     function getChecklists() {
-//       return ChecklistFactory.getChecklists()
-//         .then(setChecklists)
-//     }
-//
-//     function createChecklist() {
-//       return ChecklistFactory.createChecklist(vm.newChecklist)
-//       .then(getChecklists())
-//     }
-//
-//     function setChecklists(data) {
-//       return vm.checklists = data;
-//     }
-//   }]);
-//
-// }())
-;
+(function(){
+
+    'use strict';
+
+    function ChecklistsController(ChecklistFactory) {
+
+        var vm = this;
+
+        //callable methods on the vm
+        vm.test = "Here is the checklist!";
+        vm.createNote = createNote;
+        activate();
+        //defined methods on the vm
+        function activate() {
+            getChecklists();
+        };
+
+        function getChecklists() {
+            return ChecklistFactory.getChecklists()
+                .then(setChecklists);
+        };
+
+        function createChecklist() {
+            return ChecklistFactory.createChecklist(vm.Checklist)
+                .then(getChecklists());
+        };
+
+        function setChecklists(data) {
+            return vm.checklists = data;
+        };
+
+
+    };
+
+    angular
+      .module('app')
+      .controller('ChecklistsController', ChecklistsController);
+
+}());
 // Angular Rails Template
 // source: app/assets/javascripts/checklists/show.html
 
@@ -52385,84 +52390,45 @@ angular.module("templates").run(["$templateCache", function($templateCache) {
 //       .factory('ItemFactory', ItemFactory);
 //
 // }());
-// (function(){
-//
-//   'use strict'
-//
-//   angular
-//     .module('app')
-//     .controller('ItemsController', ['ItemFactory', function(ItemFactory) {
-//
-//     var vm = this;
-//
-//     //callable methods on the vm
-//     vm.test = "Here are the items!";
-//     vm.createItem = createItem;
-//     activate();
-//
-//     //defined methods on the vm
-//     function activate() {
-//       getItems();
-//     }
-//
-//     function getItems() {
-//       return ItemFactory.getItems()
-//         .then(setItems)
-//     }
-//
-//     function createItem() {
-//       return ItemFactory.createItem(vm.Item)
-//         .then(getItems())
-//     }
-//
-//     function setItems(data) {
-//         return vm.items = data;
-//     }
-//   }]);
-//
-// }())
-//
-//
-//
-// // (function(){
-// //
-// //     'use strict';
-// //
-// //     function ItemsController(ItemFactory) {
-// //
-// //         var vm = this;
-// //
-// //         //callable methods on the vm
-// //         vm.test = "Here are the items!";
-// //         vm.createItem = createItem;
-// //         activate();
-// //         //defined methods on the vm
-// //         function activate() {
-// //             getItems();
-// //         };
-// //
-// //         function getItems() {
-// //             return ItemFactory.getItems()
-// //                 .then(setItems);
-// //         };
-// //
-// //         function createItem() {
-// //             return ItemFactory.createItem(vm.Item)
-// //                 .then(getItems());
-// //         };
-// //
-// //         function setItems(data) {
-// //             return vm.items = data;
-// //         };
-// //
-// //
-// //     };
-// //
-// //     angular
-// //       .module('app')
-// //       .controller('ItemsController', ItemsController);
-// //
-// // }());
+(function(){
+
+    'use strict';
+
+    function ItemsController(ItemFactory) {
+
+        var vm = this;
+
+        //callable methods on the vm
+        vm.test = "Here are the items!";
+        vm.createItem = createItem;
+        activate();
+        //defined methods on the vm
+        function activate() {
+            getItems();
+        };
+
+        function getItems() {
+            return ItemFactory.getItems()
+                .then(setItems);
+        };
+
+        function createItem() {
+            return ItemFactory.createItem(vm.Item)
+                .then(getItems());
+        };
+
+        function setItems(data) {
+            return vm.items = data;
+        };
+
+
+    };
+
+    angular
+      .module('app')
+      .controller('ItemsController', ItemsController);
+
+}());
 // Angular Rails Template
 // source: app/assets/javascripts/items/show.html
 
@@ -52710,12 +52676,9 @@ angular.module("templates").run(["$templateCache", function($templateCache) {
 //
 // }());
 (function() {
+  'use strict';
 
-  'use strict'
-
-  angular
-    .module('app')
-    .controller('JobsController', ['JobFactory', '$state', function(JobFactory, $state) {
+  function JobsController(JobFactory, $state) {
     var vm = this;
     console.log($state);
 
@@ -52740,45 +52703,45 @@ angular.module("templates").run(["$templateCache", function($templateCache) {
       {id: 9, value: 'Job Offer Declined'}
     ];
 
-    //instantiate functions
+
+    //instantiated info
     activate();
 
-    // define methods
+    //defined methods on the vm
     function activate() {
       if ($state.current.name == "jobs.list") {
         getJobs();
       }
-      else if  ($state.current.name == "jobs.show" || $state.current.name == "jobs.show.checklists") {
+      else if  ($state.current.name == "jobs.show") {
         getJob($state.params.id)
       }
-
-    }
+    };
 
     function getJobs() {
       return JobFactory.getJobs()
-            .then(setJobs)
-    }
+            .then(setJobs);
+    };
 
     function getJob(params) {
       return JobFactory.getJob(params)
-            .then(setJobs)
-    }
+            .then(setJob);
+    };
 
     function createJob() {
-      // debugger
+      // debugger;
       return JobFactory.createJob(vm.Job)
-            .then(showJobs)
-    }
+             .then(showJobs)
+    };
 
     function updateJob() {
       return JobFactory.updateJob(vm.Job)
-            .then(showJob)
-    }
+            .then(showJob);
+    };
 
     function destroyJob(id) {
       return JobFactory.destroyJob(id)
-            .then(showJobs)
-    }
+            .then(showJobs);
+    };
 
     function updateStatus(jobId, jobStatus) {
       return JobFactory.updateStatus(jobId, jobStatus)
@@ -52786,117 +52749,26 @@ angular.module("templates").run(["$templateCache", function($templateCache) {
 
     function setJobs(data) {
       return vm.jobs = data;
-    }
+    };
 
     function setJob(data) {
       return vm.showJob = data;
     };
 
     function showJob(data) {
-        $state.go('jobs.show', { jobId: data.id })
+        $state.go('jobs.show', { jobId: data.id });
     };
 
     function showJobs() {
-        $state.go('jobs.list')
-    }
-  }]);
+        $state.go('jobs.list');
+    };
 
-}())
+  };
 
-// (function() {
-//   'use strict';
-//
-//   function JobsController(JobFactory, $state) {
-//     var vm = this;
-//     console.log($state);
-//
-//     // callable methods on the vm
-//     vm.test = "View the jobs!";
-//     vm.getJobs = getJobs;
-//     vm.createJob = createJob;
-//     vm.getJob = getJob;
-//     vm.updateJob = updateJob;
-//     vm.destroyJob = destroyJob;
-//     vm.updateStatus = updateStatus;
-//
-//     vm.statuses = [
-//       {id: 1, value: 'Discovered'},
-//       {id: 2, value: 'Application in Progress'},
-//       {id: 3, value: 'Applied'},
-//       {id: 4, value: 'Interviewing'},
-//       {id: 5, value: 'Pending Response'},
-//       {id: 6, value: 'No Response'},
-//       {id: 7, value: 'Job Offer Received'},
-//       {id: 8, value: 'Job Offer Accepted'},
-//       {id: 9, value: 'Job Offer Declined'}
-//     ];
-//
-//
-//     //instantiated info
-//     activate();
-//
-//     //defined methods on the vm
-//     function activate() {
-//       if ($state.current.name == "jobs.list") {
-//         getJobs();
-//       }
-//       else if  ($state.current.name == "jobs.show") {
-//         getJob($state.params.id)
-//       }
-//     };
-//
-//     function getJobs() {
-//       return JobFactory.getJobs()
-//             .then(setJobs);
-//     };
-//
-//     function getJob(params) {
-//       return JobFactory.getJob(params)
-//             .then(setJob);
-//     };
-//
-//     function createJob() {
-//       // debugger;
-//       return JobFactory.createJob(vm.Job)
-//              .then(showJobs)
-//     };
-//
-//     function updateJob() {
-//       return JobFactory.updateJob(vm.Job)
-//             .then(showJob);
-//     };
-//
-//     function destroyJob(id) {
-//       return JobFactory.destroyJob(id)
-//             .then(showJobs);
-//     };
-//
-//     function updateStatus(jobId, jobStatus) {
-//       return JobFactory.updateStatus(jobId, jobStatus)
-//     }
-//
-//     function setJobs(data) {
-//       return vm.jobs = data;
-//     };
-//
-//     function setJob(data) {
-//       return vm.showJob = data;
-//     };
-//
-//     function showJob(data) {
-//         $state.go('jobs.show', { jobId: data.id });
-//     };
-//
-//     function showJobs() {
-//         $state.go('jobs.list');
-//     };
-//
-//   };
-//
-//   angular
-//     .module('app')
-//     .controller('JobsController', JobsController);
-// }());
+  angular
+    .module('app')
+    .controller('JobsController', JobsController);
+}());
 // Angular Rails Template
 // source: app/assets/javascripts/jobs/show.html
 
