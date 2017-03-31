4cce06ad10587834d61151776e0e2ac115d3cfee
karuna24s <karuna24s@gmail.com>
2017-03-31 11:45:49 -0400

    attempting to correct typeerror issue by changeing factory files to older versions

--- app/assets/javascripts/checklists/checklist.factory.js
+++ app/assets/javascripts/checklists/checklist.factory.js
@@ -1,82 +1,100 @@
-(function() {
-
-  'use strict'
-
-  angular
-    .module('app')
-    .factory('ChecklistFactory', ['$http', '$state', function($http, $state) {
-
-    return {
-      getChecklists: getChecklists,
-      createChecklist: createChecklist
-    }
-
-    function getChecklists() {
-      return $http.get('/jobs/' + $state.param.id + '/checklists')
-        .then(handleSuccess)
-        .then(handleError)
-    }
-
-    function createChecklist(checklist) {
-      var req = {
-        method: 'POST',
-        url: '/jobs/' + $state.params.id + '/checklists',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            checklist: checklist
-        }
-      }
-
-      return $http(req)
-        .then(handleSuccess)
-        .catch(handleError)
-    }
-
-    function handleSuccess(response) {
-      console.log(response);
-      return response.data;
-    }
-
-    function handleError(error) {
-      console.log(error);
-    }
-  }]);
-
-}())
-
-
-
-// (function(){
+// (function() {
 //
-//     'use strict';
-//
-//     function ChecklistFactory($http) {
-//
-//         return {
-//             getChecklists: getChecklists
-//         }
-//
-//         function getChecklists() {
-//             return $http.get('/checklists')
-//                 .then(handleSuccess)
-//                 .catch(handleError)
-//         };
-//
-//         function handleSuccess(response) {
-//             console.log(response);
-//             return response.data;
-//         };
-//
-//         function handleError(error) {
-//             console.log(error);
-//         };
-//
-//     };
+//   'use strict'
 //
 //   angular
-//       .module('app')
-//       .factory('ChecklistFactory', ChecklistFactory);
+//     .module('app')
+//     .factory('ChecklistFactory', ['$http', '$state', function($http, $state) {
 //
-// }());
+//     return {
+//       getChecklists: getChecklists,
+//       createChecklist: createChecklist
+//     }
+//
+//     function getChecklists() {
+//       return $http.get('/jobs/' + $state.param.id + '/checklists')
+//         .then(handleSuccess)
+//         .then(handleError)
+//     }
+//
+//     function createChecklist(checklist) {
+//       var req = {
+//         method: 'POST',
+//         url: '/jobs/' + $state.params.id + '/checklists',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         data: {
+//             checklist: checklist
+//         }
+//       }
+//
+//       return $http(req)
+//         .then(handleSuccess)
+//         .catch(handleError)
+//     }
+//
+//     function handleSuccess(response) {
+//       console.log(response);
+//       return response.data;
+//     }
+//
+//     function handleError(error) {
+//       console.log(error);
+//     }
+//   }]);
+//
+// }())
+
+
+
+(function(){
+
+    'use strict';
+
+    function ChecklistFactory($http, $state) {
+
+        return {
+            getChecklists: getChecklists,
+            createChecklist: createChecklist
+        }
+
+        function getChecklists() {
+            return $http.get('/jobs/' + $state.params.id + '/checklists')
+                .then(handleSuccess)
+                .catch(handleError)
+        };
+
+        function createChecklist(checklist) {
+          var req = {
+              method: 'POST',
+              url: '/jobs/' + $state.params.id + '/checklists',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              data: {
+                  checklist: checklist
+              }
+          };
+
+          return $http(req)
+                     .then(handleSuccess)
+                     .catch(handleError)
+        };
+
+        function handleSuccess(response) {
+            console.log(response);
+            return response.data;
+        };
+
+        function handleError(error) {
+            console.log(error);
+        };
+
+    };
+
+  angular
+      .module('app')
+      .factory('ChecklistFactory', ChecklistFactory);
+
+}());
--- app/assets/javascripts/items/item.factory.js
+++ app/assets/javascripts/items/item.factory.js
@@ -1,81 +1,133 @@
-(function() {
+(function(){
 
-  'use strict'
+    'use strict';
+
+    function ItemFactory($http, $state) {
+
+        return {
+            getItems: getItems,
+            createItem: createItem
+        }
+
+        function getItems() {
+            return $http.get('/jobs/' + $state.params.id + '/checklists' + '/items/')
+                .then(handleSuccess)
+                .catch(handleError)
+        };
+
+        function createItem(item) {
+          var req = {
+              method: 'POST',
+              url: '/jobs/' + $state.params.id + '/checklists' + '/items/',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              data: {
+                  item: item
+              }
+          };
+
+          return $http(req)
+                     .then(handleSuccess)
+                     .catch(handleError)
+        };
+
+        function handleSuccess(response) {
+            console.log(response);
+            return response.data;
+        };
+
+        function handleError(error) {
+            console.log(error);
+        };
+
+    };
 
   angular
-    .module('app')
-    .factory('ItemFactory', ['$http', '$state', function($http, $state) {
+      .module('app')
+      .factory('ItemFactory', ItemFactory);
 
-    return {
-      getItems: getItems,
-      createItem: createItem
-    }
-
-    function getItems() {
-      return $http.get('/jobs/' + $state.params.id + '/checklists' + '/items')
-        .then(handleSuccess)
-        .then(handleError)
-    }
-
-    function createItem(item) {
-      var req = {
-        method: 'POST',
-        url: '/jobs/' + $state.params.id + '/checklists' + '/items',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            item: item
-        }
-      }
-
-      return $http(req)
-        .then(handleSuccess)
-        .catch(handleError)
-    }
-
-    function handleSuccess(response) {
-      console.log(response);
-      return response.data;
-    }
-
-    function handleError(error) {
-      console.log(error);
-    }
-  }]);
-
-}())
+}());
 
 
-// (function(){
+// (function() {
 //
-//     'use strict';
-//
-//     function ItemFactory($http) {
-//
-//         return {
-//             getItems: getItems
-//         }
-//
-//         function getItems() {
-//             return $http.get('/items')
-//                 .then(handleSuccess)
-//                 .catch(handleError)
-//         };
-//
-//         function handleSuccess(response) {
-//             console.log(response);
-//             return response.data;
-//         };
-//
-//         function handleError(error) {
-//             console.log(error);
-//         };
-//
-//     };
+//   'use strict'
 //
 //   angular
-//       .module('app')
-//       .factory('ItemFactory', ItemFactory);
+//     .module('app')
+//     .factory('ItemFactory', ['$http', '$state', function($http, $state) {
 //
-// }());
+//     return {
+//       getItems: getItems,
+//       createItem: createItem
+//     }
+//
+//     function getItems() {
+//       return $http.get('/jobs/' + $state.params.id + '/checklists' + '/items')
+//         .then(handleSuccess)
+//         .then(handleError)
+//     }
+//
+//     function createItem(item) {
+//       var req = {
+//         method: 'POST',
+//         url: '/jobs/' + $state.params.id + '/checklists' + '/items',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         data: {
+//             item: item
+//         }
+//       }
+//
+//       return $http(req)
+//         .then(handleSuccess)
+//         .catch(handleError)
+//     }
+//
+//     function handleSuccess(response) {
+//       console.log(response);
+//       return response.data;
+//     }
+//
+//     function handleError(error) {
+//       console.log(error);
+//     }
+//   }]);
+//
+// }())
+//
+//
+// // (function(){
+// //
+// //     'use strict';
+// //
+// //     function ItemFactory($http) {
+// //
+// //         return {
+// //             getItems: getItems
+// //         }
+// //
+// //         function getItems() {
+// //             return $http.get('/items')
+// //                 .then(handleSuccess)
+// //                 .catch(handleError)
+// //         };
+// //
+// //         function handleSuccess(response) {
+// //             console.log(response);
+// //             return response.data;
+// //         };
+// //
+// //         function handleError(error) {
+// //             console.log(error);
+// //         };
+// //
+// //     };
+// //
+// //   angular
+// //       .module('app')
+// //       .factory('ItemFactory', ItemFactory);
+// //
+// // }());
