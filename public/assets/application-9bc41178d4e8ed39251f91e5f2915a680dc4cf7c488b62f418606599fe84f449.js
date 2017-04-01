cecb4ce7d12ed8d26d2a9efd0e5e634f9b451e86
karuna24s <karuna24s@gmail.com>
2017-03-31 20:35:06 -0400

    precompiled assets for production after creating Authcontroller

--- public/assets/application-9bc41178d4e8ed39251f91e5f2915a680dc4cf7c488b62f418606599fe84f449.js
+++ public/assets/application-9bc41178d4e8ed39251f91e5f2915a680dc4cf7c488b62f418606599fe84f449.js
@@ -51532,6 +51532,37 @@ function ngMessageDirectiveFactory() {
     }]);
 }())
 ;
+(function(){
+
+    'use strict';
+
+    function AuthController($scope, $state, Auth) {
+
+        //callable methods on the vm
+        vm.login = login;
+        vm.register = register;
+
+        //defined methods on the vm
+        function login() {
+            Auth.login($scope.user)
+                .then(goToJobs);
+        };
+
+        function register() {
+            Auth.register($scope.user)
+                .then(goToJobs);
+        };
+
+        function goToJobs() {
+            $state.go('home.jobs');
+        };
+
+    };
+
+    angular
+        .module('app')
+        .controller('authController', AuthController);
+}());
 (function() {
   (function() {
     (function() {
