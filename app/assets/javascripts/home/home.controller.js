9585012d6bcade562862c1fa7884c6d7c3e80d5d
karuna24s <karuna24s@gmail.com>
2017-03-31 20:09:45 -0400

    add navbar HomeController with angular-devise login/logout

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -1,20 +1,42 @@
 (function() {
+  
   'use strict';
 
-  function HomeController() {
+  function HomeController(Auth) {
     var vm = this;
 
     // callable methods on the vm
-    // vm.name = 'Karuna'
-    // vm.test = "Let's see if this shows!";
+    vm.signedIn = Auth.isAuthenticated;
+    vm.logout = Auth.logout;
 
     //instantiated info
     activate();
 
     //defined methods on the vm
     function activate() {
-
+        getCurrentUser();
     };
+
+    function getCurrentUser() {
+        return Auth.currentUser()
+                   .then(setCurrentUser);
+    };
+
+    function setCurrentUser(user) {
+        console.log(user);
+        return vm.user = user;
+    };
+
+    //event listeners for user authentication and logout
+    $scope.$on('devise:new-registration', function(e, user){
+        return vm.user = user;
+    });
+    $scope.$on('devise:login', function(e, user){
+        return vm.user = user;
+    });
+    $scope.$on('devise:logout', function(e, user){
+        return vm.user = {};
+    });
 
   };
 
